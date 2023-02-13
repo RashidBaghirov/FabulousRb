716f1333a22310dd3da053174666301579119ec4
RashidBaghirov <rashidtb@code.edu.az>
2023-02-13 11:37:41 +0400

    cart 75% is done

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -42,4 +42,13 @@ delbtn.forEach((btn) => {
   ShowProductCount(device);
   localStorage.setItem("device", JSON.stringify(device));
   });
-  });
+ });
+
+
+ $(".cart_info_faq_title").click(function () {
+  let $icon = $(this).find("i");
+  $icon.toggleClass("active");
+  $(this).next().slideToggle();
+  $(".cart_info_").not($(this).next()).slideUp();
+  $(".cart_info_faq_title i").not($icon).removeClass("active");
+});
