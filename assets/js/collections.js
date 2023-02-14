ecd3c4050870283824c7256687686799ffaf8810
RashidBaghirov <rashidtb@code.edu.az>
2023-02-14 13:05:03 +0400

    header add link all page

--- assets/js/collections.js
+++ assets/js/collections.js
@@ -6,3 +6,15 @@ $(".device").on("mouseover", function () {
     $(this).find(".add_cart").removeClass("active");
     $(this).find(".allbutton").removeClass("active");
   });
+
+
+  $(".grid .grid_grip").click(function () {
+    $(".second_page").fadeOut();
+    $(".first_page").fadeIn();
+  
+  });
+
+  $(".grid .list").click(function () {
+    $(".first_page").fadeOut();
+    $(".second_page").fadeIn();
+  });
