34ed656da5cc088d260b54c641dc447a06efc018
RashidBaghirov <rashidtb@code.edu.az>
2023-02-14 17:16:04 +0400

    HTCTouch halp part is done

--- assets/js/htc.js
+++ assets/js/htc.js
@@ -35,3 +35,13 @@ $(".responsive").slick({
       },
     ],
   });
+
+
+
+  $(".htc_title").click(function () {
+    
+    $(this).toggleClass("active");
+    $(this).next().slideToggle();
+    $(".htc_info").not($(this).next()).slideUp();
+    // $(".htc_title").removeClass("active");
+  });
